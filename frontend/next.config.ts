7e836c7636701da2fa044df08ba5dcfeb38fa612
animesh65432 <kiranduttta234@gmail.com>
2024-11-24 23:45:56 +0530

    fix the issues

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+const port = process.env.PORT || 3000;
+
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverRuntimeConfig: {
+    port, // Ensures the server binds to the correct port
+  },
 };
 
 export default nextConfig;
