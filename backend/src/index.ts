1ad24ef293a59628d1f77779a942ac2a2a8b414e
animesh65432 <kiranduttta234@gmail.com>
2024-11-25 12:18:39 +0530

    add frontend url in the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import cors from "cors"
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL as string,
     credentials: true
 }))
 app.use(express.json())
