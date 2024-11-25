433cc8768639ee70d2d22097fc582a390b8fded7
animesh65432 <kiranduttta234@gmail.com>
2024-11-25 11:57:22 +0530

    fix the boundery issues

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import { StoreProvider } from "../components"
 import { Toaster } from "@/components/ui/toaster"
+import { Suspense } from "react"
 
 export default function RootLayout({
   children,
@@ -13,10 +14,13 @@ export default function RootLayout({
       <body
       >
         <StoreProvider>
-          <main>
-            {children}
-            <Toaster />
-          </main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <main>
+
+              {children}
+              <Toaster />
+            </main>
+          </Suspense>
         </StoreProvider>
       </body>
     </html>
